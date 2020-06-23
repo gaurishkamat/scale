240ebf4b144000efe4935fee7038e1b99d57af08
Rómulo Lazarde <romulo.lazarde@iconstorm.com>
2020-06-23 16:55:39 +0200

    update: Adds better docs integration to storybook  Changes to be committed: 	deleted:    examples/boilerplate-gatsby/yarn.lock 	deleted:    examples/boilerplate-nuxt/yarn.lock 	deleted:    examples/boilerplate-react/yarn.lock 	deleted:    examples/boilerplate-vue/yarn.lock 	modified:   packages/components/.storybook/main.js 	new file:   packages/components/.storybook/preview.js 	modified:   packages/components/package.json 	modified:   packages/components/src/components/button/button.stories.js 	new file:   packages/components/src/components/button/button.stories.mdx 	modified:   packages/components/src/components/button/readme.md 	modified:   packages/components/src/components/button/usage/button-accessibility.md 	modified:   yarn.lock

--- packages/components/src/components/button/button.stories.js
+++ packages/components/src/components/button/button.stories.js
@@ -1,36 +1,72 @@
-import { storiesOf } from '@storybook/html'
-import { withKnobs, text, select } from '@storybook/addon-knobs'
-import { withActions } from '@storybook/addon-actions'
-import { h } from 'jsx-dom'
+import { withKnobs, text, select, optionsKnob as options } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
 
-import readme from './readme.md'
+import { h } from 'jsx-dom';
 
-storiesOf('Scale Button', module)
-    .addDecorator(withActions('onClick'))
-    .addDecorator(withKnobs)
-    .add(
-        'Default',
-        () => {
-            const label = text('label', 'Label');
-            const optionsVariant = {
-                'Default': '',
-                'Primary': 'primary',
-                'Secondary': 'secondary',
-                'Success': 'success',
-                'Error': 'error',
-                'Warning': 'warning',
-                'Info': 'info'
-            }
-            const variant = select('Variant', optionsVariant, 'Default')
-            return (
-            <scale-button
-                variant={variant}
-            >{label}</scale-button>
-            )
-        },
-        {
-            notes: {
-                markdown: readme,
-            }
-        }
-    )
+export default {
+  title: 'Components/Button',
+  component: 'scale-button',
+  decorators: [ withKnobs, withA11y],
+  includeStories: []
+}
+
+export const playground = () => {
+    // BUTTON VALUE
+    const buttonValueLabel = 'Button Value';
+    const buttonValueDefault = 'Standard';
+    const buttonValue = text(buttonValueLabel, buttonValueDefault);
+
+  // BUTTON VARIANT
+  const variantLabel = 'Button Variant';
+  const variantOptions = {
+    Default: '',
+    Primary: 'primary',
+    Secondary: 'secondary',
+    Success: 'success',
+    Error: 'error',
+    Warning: 'warning',
+    Info: 'info'
+  };
+  const variantDefault = 'Default';
+  const variant = select(variantLabel, variantOptions, variantDefault);
+
+  return (
+    <scale-button
+      variant={variant}
+  >{buttonValue}</scale-button>
+  )
+}
+playground.story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<scale-button variant="variant" href="url">PLAYGROUND</storm-button>'
+      }
+    }
+  },
+  includeStories: []
+}
+
+export const primary = () => {
+return (
+    <scale-button
+      variant="primary">Primary</scale-button>
+  )
+}
+primary.story = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<scale-button variant="variant" href="url">PRIMARY</storm-button>'
+      }
+    }
+  },
+  includeStories: []
+}
+
+export const secondary = () => {
+return (
+    <scale-button
+      variant="secondary">Secondary</scale-button>
+  )
+}
