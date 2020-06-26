bff7bed588d4c67e0dd0c277c530801001076bd7
Daniel Beck <nowseemee@gmail.com>
2020-06-26 19:37:02 +0200

    fix: do not polyfill animations while SSR

--- packages/components/global.ts
+++ packages/components/global.ts
@@ -1,3 +1,6 @@
-import 'web-animations-js';
+if (typeof window !== 'undefined' && typeof window.Audio !== 'undefined') {
+  // tslint:disable-next-line:no-var-requires
+  require('web-animations-js');
+}
 
 export default () => null;
