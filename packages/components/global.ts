e7bd1787c0de0b2d826cac0392ad731b22a66fc1
Daniel Beck <nowseemee@gmail.com>
2020-06-29 12:59:08 +0200

    fix: safe-guard require statement

--- packages/components/global.ts
+++ packages/components/global.ts
@@ -1,4 +1,8 @@
-if (typeof window !== 'undefined' && typeof window.Audio !== 'undefined') {
+if (
+  typeof window !== 'undefined' &&
+  typeof window.Audio !== 'undefined' &&
+  typeof require !== 'undefined'
+) {
   // tslint:disable-next-line:no-var-requires
   require('web-animations-js');
 }
