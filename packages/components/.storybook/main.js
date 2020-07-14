f0cf007f81922d5b6a6ffdf14bf154c2dafd8192
Arturo Castillo <ac@iconstorm.com>
2020-07-14 14:02:34 +0000

    chore: remove _usage_ folder from button component since Storybook will provide that information now

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,6 +1,8 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.*"
+  stories: [
+    '../src/**/*.stories.*'
   ],
   "addons": [
     "@storybook/addon-links",
@@ -9,5 +11,21 @@ module.exports = {
     "@storybook/addon-actions",
     "@storybook/addon-a11y",
     "@storybook/addon-viewport"
-  ]
-}
+  ],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    /* config.module.rules.push({
+      test: /\.(ttf|eot|woff|woff2)$/,
+      use: {
+        loader: 'file-loader',
+        options: { name: '[name].[ext]' }
+      },
+      include: path.resolve(__dirname, '../'),
+    }); */
+
+    return config;
+  },
+};
