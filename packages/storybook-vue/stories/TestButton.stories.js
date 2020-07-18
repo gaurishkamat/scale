2ecab6c470945fb29e2c92ce78d0247f8d72dae3
Arturo <ac@iconstorm.com>
2020-07-18 12:39:50 +0200

    wip: test Storybook args with Vue

--- packages/storybook-vue/stories/TestButton.stories.js
+++ packages/storybook-vue/stories/TestButton.stories.js
@@ -1,13 +1,24 @@
 import { action } from "@storybook/addon-actions";
 import TestButton from './TestButton.vue';
+import { Template } from './TestButton.stories.mdx';
 
 export default {
   title: "Test Button",
   component: TestButton
 };
 
+/* const Template = (args) => ({
+  components: { TestButton },
+  data() {
+    return { args }
+  },
+  template: '<test-button v-bind="args"></test-button>'
+}) */
+
 export const Text = () => ({
   components: { TestButton },
   template: '<test-button @click.native="action" label="Click!" />',
   methods: { action: action("clicked") }
 });
+
+export const WithArgs = Template.bind(null)
