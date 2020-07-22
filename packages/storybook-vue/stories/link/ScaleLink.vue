e6b86a61cc09116ef2dc8cb27625ad2f0a4061df
Juliana Rocha <jcr@iconstorm.com>
2020-07-22 15:23:44 +0200

    feature: add story for the link component

---
+++ packages/storybook-vue/stories/link/ScaleLink.vue
@@ -0,0 +1,31 @@
+<template>
+    <scale-link
+        :block="block"
+        :custom-lass="customClass"
+        :disabled="disabled"
+        :href="href"
+        :icon="icon"
+        :icon-size="iconSize"
+        :target="target"
+        :underline="underline"
+        :variant="variant"
+    >
+        <slot></slot>
+    </scale-link>
+</template>
+
+<script>
+export default {
+    props: {
+        block: {type: Boolean, default: false },
+        customClass: String,
+        disabled: {type: Boolean, default: false},
+        href: String,
+        icon: String,
+        iconSize: {type: Number, default: 24},
+        target: {type: String, default: '_self'},
+        underline: {type: Boolean, default: true},
+        variant: String
+    }
+}
+</script>
