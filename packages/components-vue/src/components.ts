71f47817e21cdb9c5f6b25c83f1b710ad751b454
Juliana Rocha <jcr@iconstorm.com>
2020-07-24 12:14:47 +0200

    WIP: add props and snippets to tag component and to switch

--- packages/components-vue/src/components.ts
+++ packages/components-vue/src/components.ts
@@ -38,6 +38,9 @@ const customElementTags: string[] = [
  'scale-slider',
  'scale-ssr-slot-fix',
  'scale-switch',
+ 'scale-tab-header',
+ 'scale-tab-nav',
+ 'scale-tab-panel',
  'scale-table',
  'scale-tag',
  'scale-text',
@@ -507,6 +510,48 @@ export const ScaleSwitch = /*@__PURE__*/ Vue.extend({
 
 
   render: createCommonRender('scale-switch', []),
+});
+
+
+export const ScaleTabHeader = /*@__PURE__*/ Vue.extend({
+
+  props: {
+    styles: {} as PropOptions<Components.ScaleTabHeader['styles']>,
+    label: {} as PropOptions<Components.ScaleTabHeader['label']>,
+    ariaLabel: {} as PropOptions<Components.ScaleTabHeader['ariaLabel']>,
+    identifier: {} as PropOptions<Components.ScaleTabHeader['identifier']>,
+    selected: {} as PropOptions<Components.ScaleTabHeader['selected']>,
+    icon: {} as PropOptions<Components.ScaleTabHeader['icon']>,
+    iconSize: {} as PropOptions<Components.ScaleTabHeader['iconSize']>,
+  },
+
+
+  render: createCommonRender('scale-tab-header', ['tabclick']),
+});
+
+
+export const ScaleTabNav = /*@__PURE__*/ Vue.extend({
+
+  props: {
+    styles: {} as PropOptions<Components.ScaleTabNav['styles']>,
+    ariaLabel: {} as PropOptions<Components.ScaleTabNav['ariaLabel']>,
+  },
+
+
+  render: createCommonRender('scale-tab-nav', []),
+});
+
+
+export const ScaleTabPanel = /*@__PURE__*/ Vue.extend({
+
+  props: {
+    styles: {} as PropOptions<Components.ScaleTabPanel['styles']>,
+    ariaLabel: {} as PropOptions<Components.ScaleTabPanel['ariaLabel']>,
+    identifier: {} as PropOptions<Components.ScaleTabPanel['identifier']>,
+  },
+
+
+  render: createCommonRender('scale-tab-panel', []),
 });
 
 
