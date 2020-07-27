a35faf665b176e5ad2590b8f5122241e5b9af53c
Daniel Beck <nowseemee@gmail.com>
2020-07-27 20:12:27 +0200

    feat: progress-bar stories

---
+++ packages/storybook-vue/stories/3_components/progress-bar/ScaleProgressBar.vue
@@ -0,0 +1,38 @@
+<template>
+    <scale-progress-bar
+      :icon="icon"
+      :custom-class="customClass"
+      :custom-color="customColor"
+      :disabled="disabled"
+      :has-error="hasError"
+      :status-inside="statusInside"
+      :status-description="statusDescription"
+      :show-status="showStatus"
+      :stroke-width="strokeWidth"
+      :percentage="percentage"
+      :styles="styles"
+    >
+    </scale-progress-bar>
+</template>
+
+<script>
+  export default {
+    props: {
+      icon: String,
+      customClass: String,
+      customColor: String,
+      disabled: { type: Boolean, default: false },
+      statusInside: Boolean,
+      hasError: Boolean,
+      statusDescription: String,
+      styles: String,
+      showStatus: { type: Boolean, default: true },
+      strokeWidth: Number,
+      percentage: Number,
+    }
+  }
+</script>
+
+
+
+
