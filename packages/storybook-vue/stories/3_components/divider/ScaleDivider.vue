f0408a61b0961a0a648147d8749141fede5c6d5c
Daniel Beck <nowseemee@gmail.com>
2020-07-28 12:40:49 +0000

    feat: divider stories

---
+++ packages/storybook-vue/stories/3_components/divider/ScaleDivider.vue
@@ -0,0 +1,18 @@
+<template>
+  <scale-divider
+    :custom-class="customClass"
+    :vertical="vertical"
+    :styles="styles"
+  >
+  </scale-divider>
+</template>
+
+<script>
+export default {
+  props: {
+    customClass: String,
+    vertical: Boolean,
+    styles: String
+  }
+};
+</script>
