8b1734c498c9d817c6fab9bf937563df7b3f5a55
Egor <egor.kirpichev@gmail.com>
2020-08-06 12:39:54 +0200

    fix react-example fonts

--- examples/boilerplate-react/src/index.tsx
+++ examples/boilerplate-react/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { defineCustomElements } from '@scaleds/components-telekom/loader';
 import App from './App';
+import '@scaleds/components-telekom/dist/fonts/scale-fonts-telekom.css';
 import './index.css';
 
 defineCustomElements(window)
