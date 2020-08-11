0543572fb9bf86e6e66ccba98ab36ef5e0abb72e
Daniel Beck <nowseemee@gmail.com>
2020-08-11 13:33:34 +0000

    feat: split input stories

---
+++ packages/storybook-vue/stories/3_components/radio-button/ScaleRadioButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <scale-input
+    :name="name"
+    :type="type"
+    :label="label"
+    :disabled="disabled"
+    :pre-checked="preChecked"
+    :value="value"
+    :input-id="inputId"
+    @scaleChange="scaleChange"
+  >
+    <slot></slot>
+  </scale-input>
+</template>
+
+<script>
+import { action } from "@storybook/addon-actions";
+
+export default {
+  props: {
+    name: { type: String },
+    type: { type: String },
+    label: { type: String },
+    disabled: { type: Boolean },
+    preChecked: { type: Boolean },
+    value: { type: String },
+    inputId: { type: String },
+    styles: { type: String }
+  },
+  methods: {
+    scaleChange($event) {
+      action("scaleChange");
+      this.$emit("scaleChange", $event);
+    }
+  }
+};
+</script>
---
+++ packages/storybook-vue/stories/3_components/select/select.md
@@ -0,0 +1,3 @@
+# Scale Select
+
+Bacon ipsum dolor amet strip steak andouille short ribs pancetta meatloaf alcatra sausage shankle swine ham hock pork belly corned beef beef cupim sirloin. Tongue sirloin andouille chuck pork biltong bacon ball tip cow cupim.
