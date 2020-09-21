1a8d5b2a5eb72a6f7ba71a2f18048aebf6a8562d
Arturo <ac@iconstorm.com>
2020-09-21 16:54:40 +0200

    feat(components): use tokens in List (WIP)

--- packages/components/src/components/list/list.styles.ts
+++ packages/components/src/components/list/list.styles.ts
@@ -2,6 +2,20 @@ import { JssStyle } from 'jss';
 
 export const styles: JssStyle = {
   list: {
-    paddingLeft: '1.5rem',
+    paddingLeft: ({ spacing }) => spacing.flow,
+  },
+  // WIP This only works *without* `scale-list-item`
+  'list--variant-ordered': {
+    // listStyle: 'none',
+    // paddingLeft: 0,
+    // counterReset: 'scale-counter',
+    '& ::slotted(li)': {
+      counterIncrement: 'scale-counter',
+    },
+    '& ::slotted(li)::before': {
+      content: 'counter(scale-counter) "."',
+      fontFamily: 'TeleNeoWeb-Bold', // TODO use token (`weight` directly)
+      paddingRight: ({ spacing }) => spacing.inline,
+    }
   },
 };
