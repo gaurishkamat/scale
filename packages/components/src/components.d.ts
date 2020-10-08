71ab116e4c366edb8046a2424815eda3b727daa4
Arturo <ac@iconstorm.com>
2020-10-08 11:54:39 +0200

    fix(components) leaky styles and _checked_ state for Switch

--- packages/components/src/components.d.ts
+++ packages/components/src/components.d.ts
@@ -1722,7 +1722,7 @@ declare namespace LocalJSX {
         /**
           * Emitted when the switch was clicked
          */
-        "onScaleChange"?: (event: CustomEvent<void>) => void;
+        "onScaleChange"?: (event: CustomEvent<any>) => void;
         /**
           * (optional) Injected jss styles
          */
