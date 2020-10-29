935903dfe47a54e1a55c5ed83dcf80c31af47b7e
Daniel Beck <nowseemee@gmail.com>
2020-10-29 10:19:29 +0100

    chore(storybook-vue): make sure brand header config has unique IDs

--- packages/storybook-vue/stories/3_components/brand-header/fixtures.js
+++ packages/storybook-vue/stories/3_components/brand-header/fixtures.js
@@ -10,91 +10,91 @@ export const mainNavigation = [
     children: [
       {
         name: "Second Level",
-        id: "Second Level",
+        id: "Second Level32",
         children: [
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Leveln", href: "#third-level" }
+          { name: "Third Level", id: "Third Level1", href: "#third-level" },
+          { name: "Third Level", id: "Third Level2", href: "#third-level" },
+          { name: "Third Level", id: "Third Level3", href: "#third-level" },
+          { name: "Third Level", id: "Third Level4", href: "#third-level" },
+          { name: "Third Level", id: "Third Level5", href: "#third-level" },
+          { name: "Third Level", id: "Third Level6", href: "#third-level" },
+          { name: "Third Level", id: "Third Level7", href: "#third-level" }
         ]
       },
       {
         name: "Second Level",
-        id: "Second Level",
+        id: "Second Level33",
         children: [
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" }
+          { name: "Third Level", id: "Third Level8", href: "#third-level" },
+          { name: "Third Level", id: "Third Level9", href: "#third-level" },
+          { name: "Third Level", id: "Third Level10", href: "#third-level" },
+          { name: "Third Level", id: "Third Level11", href: "#third-level" },
+          { name: "Third Level", id: "Third Level12", href: "#third-level" },
+          { name: "Third Level", id: "Third Level13", href: "#third-level" },
+          { name: "Third Level", id: "Third Level14", href: "#third-level" }
         ]
       },
       {
         name: "Second Level",
-        id: "Second Level",
+        id: "Second Level34",
         children: [
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" }
+          { name: "Third Level", id: "Third Level15", href: "#third-level" },
+          { name: "Third Level", id: "Third Level16", href: "#third-level" },
+          { name: "Third Level", id: "Third Level17", href: "#third-level" },
+          { name: "Third Level", id: "Third Level18", href: "#third-level" },
+          { name: "Third Level", id: "Third Level19", href: "#third-level" },
+          { name: "Third Level", id: "Third Level20", href: "#third-level" },
+          { name: "Third Level", id: "Third Level21", href: "#third-level" }
         ]
       },
       {
         name: "Second Level",
-        id: "Second Level",
+        id: "Second Level35",
         children: [
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" }
+          { name: "Third Level", id: "Third Level22", href: "#third-level" },
+          { name: "Third Level", id: "Third Level23", href: "#third-level" },
+          { name: "Third Level", id: "Third Level24", href: "#third-level" },
+          { name: "Third Level", id: "Third Level25", href: "#third-level" },
+          { name: "Third Level", id: "Third Level26", href: "#third-level" },
+          { name: "Third Level", id: "Third Level27", href: "#third-level" }
         ]
       },
-      { name: "Second Level", id: "Second Level", href: "#second-level" }
+      { name: "Second Level", id: "Second Level28", href: "#second-level" }
     ]
   },
   {
     name: "Topic Two",
-    id: "Topic Two",
+    id: "Topic Two36",
     children: [
       {
         name: "Second Level",
-        id: "Second Level",
+        id: "Second Level37",
         children: [
-          { name: "Third Level", id: "Third Level", href: "#third-level" },
-          { name: "Third Level", id: "Third Level", href: "#third-level" }
+          { name: "Third Level", id: "Third Level29", href: "#third-level" },
+          { name: "Third Level", id: "Third Level30", href: "#third-level" }
         ]
       },
       {
         name: "Second Level",
-        id: "Second Level",
+        id: "Second Level38",
         children: [
-          { name: "Third Level", id: "Third Level", href: "#third-level" }
+          { name: "Third Level", id: "Third Level31", href: "#third-level" }
         ]
       }
     ]
   },
-  { name: "Topic Three", id: "Topic Three", href: "#topic-three" },
-  { name: "Topic Four", id: "Topic Four", href: "#topic-four" },
-  { name: "Topic Five", id: "Topic Five", href: "#topic-five" }
+  { name: "Topic Three", id: "Topic Three32", href: "#topic-three" },
+  { name: "Topic Four", id: "Topic Four33", href: "#topic-four" },
+  { name: "Topic Five", id: "Topic Five34", href: "#topic-five" }
 ];
 export const iconNavigation = [
-  { name: "Search", id: "Search", href: "#search", icon: "search" },
-  { name: "Cart", id: "Cart", href: "#cart", icon: "shopping-bag" },
-  { name: "Login", id: "Login", href: "#login", icon: "profile" },
+  { name: "Search", id: "Search35", href: "#search", icon: "search" },
+  { name: "Cart", id: "Cart36", href: "#cart", icon: "shopping-bag" },
+  { name: "Login", id: "Login37", href: "#login", icon: "profile" },
   { id: "menu", defaultName: "Menu", openedName: "Close" }
 ];
 
 export const sectorNavigation = [
-  { name: "Personal", id: "Personal", href: "#personal" },
-  { name: "Business", id: "Business", href: "#business" }
+  { name: "Personal", id: "Personal38", href: "#personal" },
+  { name: "Business", id: "Business39", href: "#business" }
 ];
