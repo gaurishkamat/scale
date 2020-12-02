cd87c7d81d085f4035fea560368d963d64520306
Arturo <ac@iconstorm.com>
2020-12-02 12:12:20 +0100

    fix: HCM border for button (bad selector)

--- packages/components/src/components/button/button.styles.ts
+++ packages/components/src/components/button/button.styles.ts
@@ -36,21 +36,21 @@ export const styles: JssStyle = {
     '&$button--icon-after ::slotted(*)': {
       marginLeft: ({ spacing }) => spacing[2],
     },
-  },
 
-  // Accessibility: Windows High Contrast Mode transparent border
-  '&:after': {
-    content: '""',
-    display: 'block',
-    boxSizing: 'border-box',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    border: '1px solid transparent',
-    borderRadius: ({ radii }) => radii.button,
-    pointerEvents: 'none',
+    // Accessibility: Windows High Contrast Mode transparent border
+    '&:after': {
+      content: '""',
+      display: 'block',
+      boxSizing: 'border-box',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      border: '1px solid transparent',
+      borderRadius: ({ radii }) => radii.button,
+      pointerEvents: 'none',
+    },
   },
 
   'button--size-small': {
