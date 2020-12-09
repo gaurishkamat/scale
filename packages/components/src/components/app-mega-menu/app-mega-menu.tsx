4c7cf1770e3cc1f644083d5753b4fa04f043cbe6
Daniel Beck <nowseemee@gmail.com>
2020-12-09 12:33:41 +0100

    fix: remove extra level of list

--- packages/components/src/components/app-mega-menu/app-mega-menu.tsx
+++ packages/components/src/components/app-mega-menu/app-mega-menu.tsx
@@ -17,41 +17,39 @@ export class MegaMenu {
         <div class="mega-menu__wrapper">
           <ul class="mega-menu__container">
             {this.navigation.map(child => (
-              <li>
-                <ul class="mega-menu__row">
-                  <li class="mega-menu__row-title">{child.name}</li>
-                  <ul>
-                    {child.children &&
-                      child.children.length > 0 &&
-                      child.children.map(menuItem => (
-                        <li>
-                          <a
-                            class={`mega-menu__row-item ${
-                              this.activeRouteId === menuItem.id ? 'active' : ''
-                            }`}
-                            aria-current={this.activeRouteId === menuItem.id}
-                            href={menuItem.href || 'javascript:void(0);'}
-                            tabIndex={this.isActive ? 0 : -1}
-                            onClick={event => {
+              <li class="mega-menu__row">
+                <li class="mega-menu__row-title">{child.name}</li>
+                <ul>
+                  {child.children &&
+                    child.children.length > 0 &&
+                    child.children.map(menuItem => (
+                      <li>
+                        <a
+                          class={`mega-menu__row-item ${
+                            this.activeRouteId === menuItem.id ? 'active' : ''
+                          }`}
+                          aria-current={this.activeRouteId === menuItem.id}
+                          href={menuItem.href || 'javascript:void(0);'}
+                          tabIndex={this.isActive ? 0 : -1}
+                          onClick={event => {
+                            this.hide();
+                            if (typeof menuItem.onClick === 'function') {
+                              menuItem.onClick(event);
+                            }
+                          }}
+                          onKeyDown={event => {
+                            if (['Escape', 'Esc'].includes(event.key)) {
                               this.hide();
-                              if (typeof menuItem.onClick === 'function') {
-                                menuItem.onClick(event);
-                              }
-                            }}
-                            onKeyDown={event => {
-                              if (['Escape', 'Esc'].includes(event.key)) {
-                                this.hide();
-                              }
-                            }}
-                          >
-                            {menuItem.name}
-                            {this.activeRouteId === menuItem.id && (
-                              <span class="sr-only">active</span>
-                            )}
-                          </a>
-                        </li>
-                      ))}
-                  </ul>
+                            }
+                          }}
+                        >
+                          {menuItem.name}
+                          {this.activeRouteId === menuItem.id && (
+                            <span class="sr-only">active</span>
+                          )}
+                        </a>
+                      </li>
+                    ))}
                 </ul>
               </li>
             ))}
