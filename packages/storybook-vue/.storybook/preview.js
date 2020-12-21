144acf8ea641c0371295c4b1fe3faa463c17cb19
Daniel Beck <nowseemee@gmail.com>
2020-12-21 11:45:16 +0100

    chore: remove dead configuration

--- packages/storybook-vue/.storybook/preview.js
+++ packages/storybook-vue/.storybook/preview.js
@@ -20,18 +20,3 @@ applyPolyfills().then(() => {
 applyPolyfillsIcons().then(() => {
   defineCustomElementsIcons(window);
 });
-
-export const globalTypes = {
-  locale: {
-    name: "Locale",
-    description: "Internationalization locale",
-    defaultValue: "en",
-    toolbar: {
-      icon: "globe",
-      items: [
-        { value: "en", title: "English" },
-        { value: "de", title: "Deutsch" }
-      ]
-    }
-  }
-};
