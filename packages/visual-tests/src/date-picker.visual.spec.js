fc85e926ab03f219a32d99e49cf15fe728c1711f
Daniel Beck <nowseemee@gmail.com>
2021-01-28 15:37:02 +0100

    test(date-picker): add visual test + snapshot

---
+++ packages/visual-tests/src/date-picker.visual.spec.js
@@ -0,0 +1,18 @@
+describe('DatePicker', () => {
+  test.each([['standard']])('%p', async variant => {
+    await global.page.goto(
+      `http://host.docker.internal:3123/iframe.html?id=components-datepicker--${variant}&viewMode=story`
+    );
+    await page.waitForSelector('html.hydrated');
+
+    const openButton = await page.evaluateHandle(
+      `document.querySelector("#root > div > scale-date-picker > duet-date-picker > div > div.duet-date__input-wrapper > button")`
+    );
+    await openButton.click();
+
+    const previewHtml = await page.$('body');
+    await page.waitFor(3000);
+
+    expect(await previewHtml.screenshot()).toMatchImageSnapshot();
+  });
+});
