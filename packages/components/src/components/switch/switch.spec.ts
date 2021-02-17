14ed8388d3e5edc5c94d8d8db569cb06f50a154b
Nathan Gordon <gordonnl@gmail.com>
2021-02-17 15:21:52 +0100

    update spec

--- packages/components/src/components/switch/switch.spec.ts
+++ packages/components/src/components/switch/switch.spec.ts
@@ -1,27 +1,12 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { Switch } from './switch';
-import { styles } from './switch.styles';
-import jss from 'jss';
 
 describe('Switch', () => {
-  let element;
-  // @ts-ignore
-  let stylesheet;
-  beforeEach(async () => {
-    element = new Switch();
-    stylesheet = element.stylesheet = jss.createStyleSheet(styles as any);
-  });
-
   it('should match snapshot', async () => {
     const page = await newSpecPage({
       components: [Switch],
       html: `<scale-switch></scale-switch>`,
     });
     expect(page.root).toMatchSnapshot();
-  });
-
-  it('should handle css classes', () => {
-    element.customClass = 'custom';
-    expect(element.getCssClassMap()).toContain('custom');
   });
 });
