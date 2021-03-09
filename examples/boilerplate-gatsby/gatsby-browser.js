f48a8904056a4e876f18f0fbc5761c7679ae3e1b
Daniel Beck <nowseemee@gmail.com>
2021-03-09 13:07:54 +0100

    docs: update example apps and docs, remove gatby-plugin

--- examples/boilerplate-gatsby/gatsby-browser.js
+++ examples/boilerplate-gatsby/gatsby-browser.js
@@ -1,8 +1,5 @@
-const { useTheme } = require("@scaleds/components-telekom/dist/theme");
 const { defineCustomElements } = require("@scaleds/components-telekom/loader");
 
-
 exports.onInitialClientRender = ({ element }, options) => {
-  useTheme(options.theme);
   defineCustomElements(window);
 };
