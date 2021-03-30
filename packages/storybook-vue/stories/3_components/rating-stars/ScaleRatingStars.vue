2d7fda57dcdef5aca8e1724c72d8f9e2163833b7
Christian Pajung <christian.pajung@telekom.de>
2021-03-30 14:28:25 +0200

    start storybook

---
+++ packages/storybook-vue/stories/3_components/rating-stars/ScaleRatingStars.vue
@@ -0,0 +1,26 @@
+<template>
+  <scale-rating-stars
+    :num-of-stars="numOfStars"
+    :rating="rating"
+    :small="small"
+    :interactive="interactive"
+    :disabled="disabled"
+    :color-fill="colorFill"
+    :aria-lang="ariaLang"
+  >
+  </scale-rating-stars>
+</template>
+
+<script>
+export default {
+  props: {
+    numOfStars: { type: Number, default: 5 },
+    rating: { type: Number, default: 3 },
+    small: { type: Boolean, default: false },
+    interactive: { type: Boolean, default: true },
+    disabled: { type: Boolean, default: false },
+    colorFill: { type: String },
+    ariaLang: { type: String },
+  },
+};
+</script>
