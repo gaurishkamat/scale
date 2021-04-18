43a3129e3c976d9acf08afc21bf5c0f51848ae12
Daniel Beck <daniel@beck.win>
2021-04-19 01:54:28 +0200

    refactor(sketch-generator): use neutral design tokens

--- packages/generator-sketch/src/index.js
+++ packages/generator-sketch/src/index.js
@@ -703,7 +703,7 @@ const dbFilename = path.resolve(__dirname, `../sketch/symbol_database.sqlite`);
     }
   }
 
-  const designTokens = await import('@scaleds/design-tokens-telekom/dist/design-tokens-telekom.js');
+  const designTokens = await import('@scaleds/design-tokens/dist/design-tokens.js');
   parseColors(sketchColors, [], designTokens.color);
 
   // Add color swatches
