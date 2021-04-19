8b1e135c466f4d30534b0c6aa8738d42b25510b5
Daniel Beck <daniel@beck.win>
2021-04-19 18:57:28 +0200

    chore: prettier

--- packages/html-to-sketch/html2asketch/helpers/svg.ts
+++ packages/html-to-sketch/html2asketch/helpers/svg.ts
@@ -6,7 +6,7 @@
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
 // based on https://www.w3.org/TR/SVG2/styling.html and defaults taken from Chrome
