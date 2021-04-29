0534147130fa4e76831829a96ccc0f25dd38b7a3
marvinLaubenstein <marvin.laubenstein@telekom.de>
2021-04-29 14:44:36 +0200

    feat: [building][first time]

--- packages/components/src/components/loading/loading.tsx
+++ packages/components/src/components/loading/loading.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Host } from '@stencil/core';
+import { Component, h, Prop } from '@stencil/core';
 
 @Component({
   tag: 'scale-loading',
@@ -6,7 +6,28 @@ import { Component, h, Host } from '@stencil/core';
   shadow: true,
 })
 export class Loading {
+  @Prop() progressAmount: string = '0';
+  @Prop() progressColor: string = '#2ecc71';
+
   render() {
-    return <Host></Host>;
+    return (
+      <div
+        style={{
+          width: `100%`,
+          height: `100%`,
+          display: `flex`,
+          alignItems: `center`,
+          justifyContent: `center`,
+          background: `conic-gradient(${this.progressColor} ${
+            this.progressAmount
+          }%, 0, #ecf0f1 ${(100 - parseInt(this.progressAmount)).toString()}%)`,
+          borderRadius: `50%`,
+        }}
+      >
+        <div>
+          <span>{this.progressAmount}</span>
+        </div>
+      </div>
+    );
   }
 }
