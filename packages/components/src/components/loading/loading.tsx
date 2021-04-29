8a2d2ce6ad9defa1095046bb50ebbe525caf1ff5
marvinLaubenstein <marvin.laubenstein@telekom.de>
2021-04-29 14:28:34 +0200

    feat: [initial commit]

---
+++ packages/components/src/components/loading/loading.tsx
@@ -0,0 +1,11 @@
+import { Component, h, Host } from '@stencil/core';
+
+@Component({
+  tag: 'scale-loading',
+  shadow: true,
+})
+export class Loading {
+  render() {
+    return <Host></Host>;
+  }
+}
